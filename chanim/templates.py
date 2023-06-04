b6cac2afe0ac87f1b0581460bdb0a9f2056456d3
Siddheshmittra <55516892+Siddheshmittra@users.noreply.github.com>
2023-06-04 18:49:51 -0400

    Update templates.py

--- chanim/templates.py
+++ chanim/templates.py
@@ -5,6 +5,99 @@ Note that setting the TeX template for a scene will affect all subsequent TexMob
 from manim.utils.tex import TexTemplate
 
 
+class ChemReactionTemplate(TexTemplate):
+    def init(self, **kwargs):
+
+        super().init(**kwargs)
+        self.add_to_preamble("\usepackage{chemfig}")
+
+
+def set_chemfig(
+        self,
+
+        ##Individual molecule params
+        atom_sep: str = "2em",  ## all of these are the defaults in chemfig
+        chemfig_style: str = "{}",
+        atom_style: str = "{}",
+        angle_increment: int = 45,
+        bond_offset: str = "2pt",
+        double_bond_sep: str = "2pt",
+        node_style: str = "{}",
+        bond_style: str = "{}",
+
+        ## Reaction scheme params
+        arrow_length: int = 1,
+        arrow_angle: int = 0,
+        arrow_style: str = "",
+        debug: str = "false",
+):
+    set_chemfig = "\\setchemfig{{angle_increment={:d},arrow angle={:d},arrow coeff={:s}".format(angle_increment,
+                                                                                                arrow_angle,
+                                                                                                arrow_length)
+    if atom_sep:
+        set_chemfig += ",atom sep={:s}".format(atom_sep)
+    if chemfig_style:
+        set_chemfig += ",chemfig style={:s}".format(chemfig_style)
+    if atom_style:
+        set_chemfig += ",atom style={:s}".format(atom_style)
+    if bond_offset:
+        set_chemfig += ",bond offset={:s}".format(bond_offset)
+    if double_bond_sep:
+        set_chemfig += ",double bond sep={:s}".format(double_bond_sep)
+    if node_style:
+        set_chemfig += ",node style={:s}".format(node_style)
+    if bond_style:
+        set_chemfig += ",bond style={:s}".format(bond_style)
+    if debug:
+        set_chemfig += ",scheme debug={:s}".format(debug)
+    if arrow_style:
+        set_chemfig += ",arrow styel={:s}".format(arrow_style)
+
+    set_chemfig += "}"
+    print(set_chemfig)
+
+    self.add_to_preamble(set_chemfig)
+
+
+def get_texcode_for_expression(self, expression):
+    """Inserts expression verbatim into TeX template.
+
+    Parameters
+    ----------
+    expression : :class:`str`
+        The string containing the expression to be typeset, e.g. ``$\\sqrt{2}$``
+
+    Returns
+    -------
+    :class:`str`
+        LaTeX code based on current template, containing the given ``expression`` and ready for typesetting
+    """
+    return self.body.replace(self.placeholder_text, "\n\\schemestart\n" + expression + "\n\\schemestop\n\n")
+
+
+def get_texcode_for_expression_in_env(self, expression, environment):
+    """Inserts an expression wrapped in a given environment into the TeX template.
+
+    Parameters
+    ----------
+    environment : :class:`str`
+        The environment in which we should wrap the expression.
+    expression : :class:`str`
+        The string containing the expression to be typeset, e.g. ``"$\\sqrt{2}$"``
+
+    Returns
+    -------
+    :class:`str`
+        LaTeX code based on template, containing the given expression and ready for typesetting
+    """
+    print(environment)
+    begin = r"\begin{" + environment + "}" + "\n\\schemestart"
+    end = "\\schemestop\n" + r"\end{" + environment + "}"
+    print(begin, end)
+    return self.body.replace(
+        self.placeholder_text, "{0}\n{1}\n{2}".format(begin, expression, end)
+    )
+
 class ChemTemplate(TexTemplate):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
@@ -91,3 +184,4 @@ class ChemReactionTemplate(TexTemplate):
         return self.body.replace(
             self.placeholder_text, "{0}\n{1}\n{2}".format(begin,expression, end)
         )
+
